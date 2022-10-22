67f186fbafd5c17f98453689b7caf50c13a4b50f
BildadOwuor <bildadowuor@gmail.com>
2022-10-22 17:36:16 +0300

    updates on user model

---
+++ lib/Models/user_model.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+
+class Students {
+  final int studentId;
+  final String studentEmail;
+  final String studentName;
+  final String studentCourseId;
+  final String studentYearOfStudy;
+
+  Students(this.studentId, this.studentEmail, this.studentName,
+      this.studentCourseId, this.studentYearOfStudy);
+  factory Students.fromReqBody(String body) {
+    Map<String, dynamic> json = jsonDecode(body);
+    return Students(
+      json['student_id'],
+      json['student_email'],
+      json['student_name'],
+      json['student_course_id'],
+      json['student_year_of_study'],
+    );
+  }
+  static fromJson(json) {
+    //returns body from firebase api call
+  }
+}
