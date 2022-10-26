2ff2da1d35e6700bc4497602d120d50c450eb0de
BildadOwuor <bildadowuor@gmail.com>
2022-10-26 21:49:44 +0300

    updates and fixes

--- lib/Views/notes.dart
+++ lib/Views/notes.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:intl/intl.dart';
+import 'package:uniapp/EditProfile/edit_profile.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({Key? key}) : super(key: key);
@@ -8,13 +10,110 @@ class NotesPage extends StatefulWidget {
   _NotesPageState createState() => _NotesPageState();
 }
 
+var year = DateFormat('y').format(DateTime.now());
+var month = DateFormat('MMM').format(DateTime.now());
+var date = DateTime.now().day;
+
 class _NotesPageState extends State<NotesPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        Text("Hi this page is about your notes"),
-      ],
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                    //color: Color(0xFFD4E7FE),
+                    gradient: LinearGradient(
+                        colors: [
+                          Color(0xFFD4E7FE),
+                          Color(0xFFF0F0F0),
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.6, 0.3])),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        const EditProfilePage()));
+                          },
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              border: Border.all(width: 1, color: Colors.white),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.blueGrey.withOpacity(0.2),
+                                  blurRadius: 12,
+                                  spreadRadius: 8,
+                                )
+                              ],
+                              image: const DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(""),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "Hi Bildad",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w900,
+                                color: Color(0XFF343E87),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Here is a list of schedule",
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.blueGrey,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              "You need to check...",
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.blueGrey,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
